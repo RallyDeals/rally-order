abde875a0571562f1c414416d5e9762c1b9493d0
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-07-21 00:55:29 +0300

    chore: wiring JWT+shared exceptions module

--- src/main/java/com/rally/order/RallyOrderApplication.java
+++ src/main/java/com/rally/order/RallyOrderApplication.java
@@ -2,8 +2,13 @@ package com.rally.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.rally.order",
+        "com.rally.common.*"
+})
 public class RallyOrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(RallyOrderApplication.class, args);
