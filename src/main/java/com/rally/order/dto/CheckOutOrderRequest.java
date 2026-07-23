84f0234fb3e6a2877bd1203d0fd44c13c1228821
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-07-23 17:30:11 +0300

    fix: add paymentIntentId field in checkoutRequest and remove UserId

--- src/main/java/com/rally/order/dto/CheckOutOrderRequest.java
+++ src/main/java/com/rally/order/dto/CheckOutOrderRequest.java
@@ -5,16 +5,14 @@ import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.List;
-import java.util.UUID;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class CheckOutOrderRequest {
-    @NotNull(message = "User id must be provided")
-    private UUID userId;
     @NotNull(message = "Order items must be provided")
     @Size(min = 1, message = "Order items must be provided")
     private List<OrderItem> orderItems;
+    private String paymentIntentId;
 }
