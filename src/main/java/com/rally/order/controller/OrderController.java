44fc0b9825bc4b58a2374a241e044a9f5e967c0d
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-07-29 22:48:42 +0300

    fix: change the import path of RequestBody annotation

--- src/main/java/com/rally/order/controller/OrderController.java
+++ src/main/java/com/rally/order/controller/OrderController.java
@@ -3,11 +3,11 @@ package com.rally.order.controller;
 import com.rally.order.dto.CheckOutOrderRequest;
 import com.rally.order.dto.CheckOutOrderResponse;
 import com.rally.order.service.NormalOrderService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +21,7 @@ public class OrderController {
     private final NormalOrderService orderService;
 
     @PostMapping("/orders/checkout")
-    public ResponseEntity<CheckOutOrderResponse> orderCheckout(@RequestHeader("X-User-Id") UUID userId, @Valid @RequestBody CheckOutOrderRequest orderRequest){
+    public ResponseEntity<CheckOutOrderResponse> orderCheckout(@RequestHeader("X-User-Id") UUID userId, @Valid @RequestBody CheckOutOrderRequest orderRequest) {
         CheckOutOrderResponse response = this.orderService.checkoutOrder(userId, orderRequest);
         return ResponseEntity.status(202).body(response);
     }
