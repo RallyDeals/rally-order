f32efcb449100ac0ffd4ec64632d6132e877ff9a
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-07-29 22:54:00 +0300

    fix: remove UUID generator annotation from eventId field, and add builder annotation on the entity

--- src/main/java/com/rally/order/messaging/processed/ProcessedEvent.java
+++ src/main/java/com/rally/order/messaging/processed/ProcessedEvent.java
@@ -4,18 +4,18 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.UuidGenerator;
+import lombok.*;
 
 import java.time.OffsetDateTime;
 
 @Entity
 @Getter @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "processed_events")
 public class ProcessedEvent {
     @Id
-    @UuidGenerator
     @Column(length = 100)
     private String eventId;
     @Column(length = 100, nullable = false)
