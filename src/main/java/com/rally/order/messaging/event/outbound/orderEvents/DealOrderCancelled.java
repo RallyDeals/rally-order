45eadc738fdd329cb84227fd66b5f4451eeff735
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-08-03 20:49:22 +0300

    fix: update event records to add @Builder annotation and remove extra fields

--- src/main/java/com/rally/order/messaging/event/outbound/orderEvents/DealOrderCancelled.java
+++ src/main/java/com/rally/order/messaging/event/outbound/orderEvents/DealOrderCancelled.java
@@ -1,8 +1,11 @@
 package com.rally.order.messaging.event.outbound.orderEvents;
 
+import lombok.Builder;
+
 import java.util.List;
 import java.util.UUID;
 
+@Builder
 public record DealOrderCancelled(UUID orderId, UUID dealId, UUID participantId, UUID userId,
-                                 List<Object> items, String reason) {
+                                 String reason) {
 }
