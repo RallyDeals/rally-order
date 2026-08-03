92e474230d19bf7b33daab1aaa57a8e234936ba1
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-08-03 23:57:07 +0300

    feat: add participation join logic

--- src/main/java/com/rally/order/mapper/OrderMapper.java
+++ src/main/java/com/rally/order/mapper/OrderMapper.java
@@ -1,8 +1,12 @@
 package com.rally.order.mapper;
 
 import com.rally.order.dto.CheckOutOrderResponse;
+import com.rally.order.dto.OrderItem;
 import com.rally.order.model.Order;
+import com.rally.order.model.OrderProduct;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
