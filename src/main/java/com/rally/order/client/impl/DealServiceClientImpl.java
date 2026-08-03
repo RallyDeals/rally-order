1363139edc379bf55fa967e499503cb420bb4a98
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-08-03 23:45:36 +0300

    feat: add deal service client with prod implementation and fake implementation

---
+++ src/main/java/com/rally/order/client/impl/DealServiceClientImpl.java
@@ -0,0 +1,66 @@
+package com.rally.order.client.impl;
+
+
+import com.rally.common.exceptions.shared.InternalServerErrorException;
+import com.rally.common.exceptions.shared.ServiceUnavailableException;
+import com.rally.order.client.DealServiceClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Profile("prod")
+@Component
+@RequiredArgsConstructor
+public class DealServiceClientImpl implements DealServiceClient {
+    private final RestTemplate restTemplate;
+
+    @Value("${deal.service.url}")
+    private String dealServiceUrl;
+
+    @Override
+    public boolean authorizeSlot(UUID dealId) {
+        String url = dealServiceUrl + "deals/" + dealId + "/authorize-slot";
+
+        try{
+            ResponseEntity<Void> response = restTemplate.postForEntity(url, null, Void.class);
+            return response.getStatusCode().is2xxSuccessful();
+        } catch (ResourceAccessException resourceAccessException) {
+            throw new ServiceUnavailableException("Deal service is unavailable");
+        } catch(HttpServerErrorException serverErrorException){
+            throw new InternalServerErrorException("Deal service returned server error");
+        }
+    }
+
+    @Override
+    public void releaseSlot(UUID dealId) {
+        String url = dealServiceUrl + "deals/" + dealId + "/release-slot";
+
+        try{
+            restTemplate.postForEntity(url, null, Void.class);
+        } catch (ResourceAccessException resourceAccessException) {
+            throw new ServiceUnavailableException("Deal service is unavailable");
+        } catch(HttpServerErrorException serverErrorException){
+            throw new InternalServerErrorException("Deal service returned server error");
+        }
+    }
+
+    @Override
+    public void releaseAuthorizedSlot(UUID dealId) {
+        String url = dealServiceUrl + "deals/" + dealId + "/release-authorized-slot";
+
+        try{
+            restTemplate.postForEntity(url, null, Void.class);
+        } catch (ResourceAccessException resourceAccessException) {
+            throw new ServiceUnavailableException("Deal service is unavailable");
+        } catch(HttpServerErrorException serverErrorException){
+            throw new InternalServerErrorException("Deal service returned server error");
+        }
+    }
+}
