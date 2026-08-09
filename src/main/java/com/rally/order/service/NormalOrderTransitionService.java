e620c7620ce617a8135086f39559f1f4e82cd0b8
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-08-09 17:33:54 +0300

    fix: match the contract returned by catalog service on lookup

--- src/main/java/com/rally/order/service/NormalOrderTransitionService.java
+++ src/main/java/com/rally/order/service/NormalOrderTransitionService.java
@@ -38,7 +38,7 @@ class NormalOrderTransitionService {
             return OrderProduct.builder()
                     .productId(item.getProductId())
                     .quantity(item.getQuantity())
-                    .unitPrice(catalogProduct.getPrice())
+                    .unitPrice(catalogProduct.getBasePrice())
                     .productName(catalogProduct.getName())
                     .productImageUrl(catalogProduct.getImageUrl())
                     .build();
