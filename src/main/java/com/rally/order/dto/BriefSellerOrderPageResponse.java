f92b87aa70605055d0a683cb732dea8a16249bd1
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-08-16 04:09:16 +0300

    feat: add endpoints for seller to review all orders that contain at least a product that they own

---
+++ src/main/java/com/rally/order/dto/BriefSellerOrderPageResponse.java
@@ -0,0 +1,19 @@
+package com.rally.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BriefSellerOrderPageResponse {
+    private List<BriefSellerOrderResponse> orders;
+    private int page;
+    private int limit;
+    private long total;
+}
